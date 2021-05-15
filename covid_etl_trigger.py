75342b2a10751379308d8f583ab782f22ef8c1b3
Arnav <arnav.gohil04@gmail.com>
2021-05-15 11:53:12 +0530

    :tada:

--- covid_etl_trigger.py
+++ covid_etl_trigger.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from operators import func
+from airflow.operators.python import PythonOperator
+from operators import DailyData, VacData
 
-func.func()
 
 default_args = {
     'owner': 'ArnavGohil',
@@ -13,29 +13,46 @@ default_args = {
     'email': ['arnav.gohil04@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 1,
     'catchup':False
 }
 
 dag = DAG('Delhi_Covid_ETL',
           default_args=default_args,
-          description='Final Project',
+          description='Near Real Time Data Processing',
           max_active_runs=1,
           schedule_interval='@once'
         )
 
+file = "{}-centers.json".format(date.strftime("%d-%m-%Y"))
+daily_data = dict()
+
+
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-get_tweet = DummyOperator(task_id='get_tweet',  dag=dag)
-get_tweet_text = DummyOperator(task_id='get_tweet_text',  dag=dag)
-parse_tweet = DummyOperator(task_id='parse_tweet',  dag=dag)
+
+get_data = PythonOperator(
+	task_id='get_data', 
+	dag=dag,
+	python_callable = DailyData.main,
+	op_kwargs={'data_dict': daily_data},
+	)
+
+
 put_data = DummyOperator(task_id='put_data',  dag=dag)
-get_centers = DummyOperator(task_id='get_centers',  dag=dag)
+
+
+get_centers = PythonOperator(
+	task_id='get_centers', 
+	dag=dag,
+	python_callable = DailyData.main,
+	op_kwargs={'file_name': file},)
+
+
 put_centers = DummyOperator(task_id='put_centers',  dag=dag)
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-start_operator >> [get_tweet,get_centers]
-get_tweet >> get_tweet_text >> parse_tweet >> put_data >> end_operator
+start_operator >> [get_data,get_centers]
+get_data >> put_data >> end_operator
 get_centers >> put_centers >> end_operator
--- operators/DailyData.py
+++ operators/DailyData.py
@@ -1,26 +1,145 @@
+import os
+import tweepy
+import configparser
+from google.cloud import vision
+import re
+import datetime
 
 
-def getTweet():
-	pass
+def initialise():
+    config = configparser.ConfigParser()
+    config.read_file(open('dwh.cfg'))
+
+    consumer_key = config.get('KEY', 'consumer_key')
+    consumer_secret = config.get('KEY', 'consumer_secret')
+    access_token = config.get('KEY', 'access_token')
+    access_token_secret = config.get('KEY', 'access_token_secret')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=config.get('KEY','json_loc')
+
+    return api
 
 
+def getTweet(api):
+    search_words = "Delhi Health Bulletin (from:CMODelhi)"
+
+    tweets = tweepy.Cursor(api.search,
+                           q=search_words,
+                           lang="en", ).items(1)
+
+    image_url = ""
+    # Iterate and print tweets
+    for tweet in tweets:
+        media = tweet.entities.get('media', [])
+    	image_url = media[0]['media_url']
+
+    return image_url
 
 
-def parseTweet():
-	pass
+def parseTweet(image_url):
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = image_url
+
+    response = client.text_detection(image=image)
+
+    string = ""
+    for text in response.text_annotations:
+        string = text.description
+        break
+
+    return string
 
 
+def getValues(val, string):
+    y = datetime.datetime.now()
+    y -= datetime.timedelta(days=1)
+    try:
+        # Date
+        match = (re.search("/ ", string))
+        i = int(match.end())
+        match = (re.search(")", string))
+        j = int(match.start())
+        print(string[i:j])
+        val['day'] = y.day
+        val['month'] = y.month
+        val['year'] = y.year
 
-def getValues():
-	pass
+        # +ve cases
+        i = string.find("Positive Cases", string.find("Positive Cases") + 1)
+        match = (re.search("Tests Conducted", string))
+        j = int(match.start())
+        val['positive'] = string[i + len("Positive Cases") + 1: j - 1]
+
+        # Tests
+        match = (re.search("Tests Conducted", string))
+        i = int(match.end())
+        match = (re.search("Positivity Rate", string))
+        j = int(match.start())
+        val['tests'] = string[i + 1:j - 1]
+
+        # Recovered
+        match = (re.search("%", string))
+        i = int(match.end())
+        match = (re.search("Deaths", string))
+        j = int(match.start())
+        val['recovered'] = string[i + 1:j - 1]
+
+        # Deaths
+        match = (re.search("Deaths", string))
+        i = int(match.end())
+        match = (re.search("COVID-19 Patient Management", string))
+        j = int(match.start())
+        val['deaths'] = string[i + 1:j - 1]
+
+        # Tot Vac
+        match = (re.search("Beneficiaries vaccinated in last 24 hours", string))
+        i = int(match.end())
+        match = (re.search("Beneficiaries vaccinated 1", string))
+        j = int(match.start())
+        val['vaccinated'] = string[i + 1:j - 1]
+
+        # 1st dose
+        match = (re.search("dose in last 24 hours", string))
+        i = int(match.end())
+        match = (re.search("Beneficiaries vaccinated 2", string))
+        j = int(match.start())
+        val['first_dose'] = string[i + 1:j - 1]
+
+        # 2nd dose
+        pat = "dose in last 24 hours"
+        pos = string.find(pat) + 1
+        p1 = string.find(pat, pos + 1)
+        i = p1 + len(pat)
+        match = (re.search("Cumulative beneficiaries vaccinated so far", string))
+        j = int(match.start())
+        val['second_dose'] = string[i + 1:j - 1]
+
+        # Active Cases
+        match = (re.search("Active Cases", string))
+        i = int(match.end())
+        match = (re.search("Total Number of Containment Zones as on date", string))
+        j = int(match.start())
+        val['active_cases'] = string[i + 1:j - 1]
+
+        # Containment
+        match = (re.search("Total Number of Containment Zones as on date: ", string))
+        i = int(match.end())
+        match = (re.search("Calls received", string))
+        j = int(match.start())
+        val['zones'] = string[i:j - 1]
+
+    except:
+        print("Error caught")
+        return None
 
 
-
-def main():
-	pass
-
-
-
-
-if __name__ == "__main__":
-    main()
+def main(data_dict, **kwargs):
+    api = initialise()
+    url = getTweet(api)
+    str = parseTweet(url)
+    getValues(data_dict, str)
